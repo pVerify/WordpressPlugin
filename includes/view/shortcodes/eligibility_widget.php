7012e1406e342f86cff61cac31396d48d8617364
webdeveloperssp <62195794+webdeveloperssp@users.noreply.github.com>
2020-11-19 16:27:12 +0530

    Add files via upload

---
+++ includes/view/shortcodes/eligibility_widget.php
@@ -0,0 +1,11 @@
+<?php 
+/**
+ * Eligibility Widget Shortcode
+ */   
+
+	if(!empty($iframe_url)){ ?>
+		<iframe width="100%" height="1000px" frameBorder="0" src="<?php echo esc_url($iframe_url); ?>"></iframe>
+	<?php }else{?>
+		<div class="errormsg_div"> <p><?php echo $err_msg; ?></p> </div>
+	<?php }
+?>

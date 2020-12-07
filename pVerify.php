704d502c1dd9d020b2a0cbe488d310264dd035d9
webdeveloperssp <62195794+webdeveloperssp@users.noreply.github.com>
2020-12-07 12:37:07 +0530

    Update pVerify.php
    pVerify file changes

--- pVerify.php
+++ pVerify.php
@@ -2,7 +2,43 @@
 /**
  * Plugin Name: pVerify
  * Plugin URI: https://wordpress.org/plugins/pverify
- * Description: This plugin integrates with our system. It will ask the user two authentication fields. Then on page load, an authentication call to our system would be performed, to get the auth token. Next, a website with the auth token as a parameter is called.
+ * Description: pVerify WP plugin to support Eligibility and Estimate Widget.
+ * Version: 0.1
+ * Author: pVerify
+ * Author URI: https://www.pverify.com/
+ * Text Domain: pverify
+ * License: later
+ *
+ * @since 0.1
+ *
+ * @package pVerify
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+require_once( ABSPATH . 'wp-admin/includes/plugin.php' );
+
+define( 'PVERIFY_PLUGIN_FILE', __FILE__ );
+
+/**
+ * Loads the action plugin
+ */
+require_once dirname( PVERIFY_PLUGIN_FILE ) . '/includes/pVerify_Main.php';
+
+pVerify_Main::instance();
+
+register_activation_hook( PVERIFY_PLUGIN_FILE, array( 'pVerify_Main', 'activate' ) );
+
+register_deactivation_hook( PVERIFY_PLUGIN_FILE, array( 'pVerify_Main', 'deactivate' ) );
+
+register_uninstall_hook( PVERIFY_PLUGIN_FILE, array( 'pVerify_Main', 'uninstall' ) ); 
+<?php
+/**
+ * Plugin Name: pVerify
+ * Plugin URI: https://wordpress.org/plugins/pverify
+ * Description: pVerify WP plugin to support Eligibility and Estimate Widget.
  * Version: 0.1
  * Author: pVerify
  * Author URI: https://www.pverify.com/
